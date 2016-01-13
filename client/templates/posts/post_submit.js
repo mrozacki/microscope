bce92556580a2b13f0028097f9b1eb2698471a9f
Maciek Rozacki <m.rozacki@gmail.com>
2016-01-13 23:38:53 +0100

    Adding Errors

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -1,13 +1,20 @@
 Template.postSubmit.events({
   "submit form": function(e){
-     e.preventDefault();
+    e.preventDefault();
 
-     var post = {
-       url: $(e.target).find("[name=url]").val(),
-       title: $(e.target).find('[name=title]').val()
-     };
+    var post = {
+      url: $(e.target).find("[name=url]").val(),
+      title: $(e.target).find('[name=title]').val()
+    };
 
-     post._id = Posts.insert(post);
-     Router.go('postPage', post);
+    Meteor.call('postInsert', post, function(error, result){
+      if (error){
+        return throwError(error.reason);
+      }
+
+      if(result.postExists)
+      throwError('This link has already been posted');
+      Router.go('postPage', {_id: result._id});
+    });
   }
 });
